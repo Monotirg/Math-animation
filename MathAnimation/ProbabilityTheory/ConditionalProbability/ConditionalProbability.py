51f2906ce1060bd873b4756e3bd2129d58d956e3
Artem <144112818+Monotirg@users.noreply.github.com>
2023-09-09 22:52:52 +0300

    Update ConditionalProbability.py

--- MathAnimation/ProbabilityTheory/ConditionalProbability/ConditionalProbability.py
+++ MathAnimation/ProbabilityTheory/ConditionalProbability/ConditionalProbability.py
@@ -3,15 +3,24 @@ from manim import *
 
 class ConditionalProbability(Scene):
     def construct(self):
-        circle_left = Circle(2, color=BLUE, stroke_width=1, fill_color=BLUE, fill_opacity=0.7).shift(RIGHT)
-        circle_right = Circle(2, color=RED,stroke_width=1 ,fill_color=RED, fill_opacity=0.8).set_z_index(circle_left.z_index-1).shift(LEFT)
-        formula = MathTex(r"P(A|B) = \frac{P(A \cap B)}{P(B)}").set_z_index(circle_left.z_index + 1)
+        circle_left = Circle(
+            radius=2, color=BLUE, stroke_width=1, 
+            fill_color=BLUE, fill_opacity=0.7
+        ).shift(RIGHT)
+        circle_right = Circle(
+            radius=2, color=RED,stroke_width=1,
+            fill_color=RED, fill_opacity=0.8
+        ).set_z_index(circle_left.z_index-1).shift(LEFT)
+        formula = MathTex(r"P(A|B) = \frac{P(A \cap B)}{P(B)}")
+        formula.set_z_index(circle_left.z_index + 1)
         VGroup(VGroup(circle_left, circle_right), formula).arrange(RIGHT, buff = 1.5)
         
         label_a = MathTex("B").move_to(circle_left.get_top()).shift(0.325*UP)
         label_b = MathTex("A").move_to(circle_right.get_top()).shift(0.325*UP)
         
-        intersection_cirlce = Intersection(circle_left, circle_right, color=GREEN, fill_color=GREEN, fill_opacity=0.5).set_z_index(circle_left.z_index + 1)
+        intersection_cirlce = Intersection(
+            circle_left, circle_right, color=GREEN, fill_color=GREEN, fill_opacity=0.5
+        ).set_z_index(circle_left.z_index + 1)
         
         self.play(DrawBorderThenFill(VGroup(circle_left, circle_right)))
         self.play(FadeIn(label_a, label_b))
