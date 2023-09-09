5e8370d411a26a94c6c273c8f9088b18bf1b69b3
Artem <144112818+Monotirg@users.noreply.github.com>
2023-09-09 23:04:58 +0300

    Update IntermediateValueTheorem.py

--- MathAnimation/Calculus/IntermediateValueTheorem/IntermediateValueTheorem.py
+++ MathAnimation/Calculus/IntermediateValueTheorem/IntermediateValueTheorem.py
@@ -15,12 +15,18 @@ class IntermediateValueTheorem(Scene):
         dot_min = Dot(axes.coords_to_point(x_min, func(x_min)), color="#FF0000")
         dot_max = Dot(axes.coords_to_point(x_max, func(x_max)), color="#FF0000")
     
-        line_min = axes.get_lines_to_point(axes.c2p(x_min,func(x_min))).set_z_index(dot_min.get_z_index()-1)
-        line_max = axes.get_lines_to_point(axes.c2p(x_max,func(x_max))).set_z_index(dot_max.get_z_index()-1)
-        label_b = MathTex("a", font_size=40).move_to(line_min[1].get_bottom()).shift(0.3*DOWN)
-        label_a = MathTex("b", font_size=40).move_to(line_max[1].get_bottom()).align_to(label_b, DOWN)
-        label_B = MathTex("A", font_size=40).move_to(line_min[0].get_left()).shift(0.3*LEFT)
-        label_A = MathTex("B", font_size=40).move_to(line_max[0].get_left()).align_to(label_B,LEFT)
+        line_min = axes.get_lines_to_point(axes.c2p(x_min,func(x_min)))
+        line_min.set_z_index(dot_min.get_z_index()-1)
+        line_max = axes.get_lines_to_point(axes.c2p(x_max,func(x_max)))
+        line_max.set_z_index(dot_max.get_z_index()-1)
+        label_b = MathTex("a", font_size=40).move_to(line_min[1].get_bottom())
+        label_b.shift(0.3*DOWN)
+        label_a = MathTex("b", font_size=40).move_to(line_max[1].get_bottom())
+        label_a.align_to(label_b, DOWN)
+        label_B = MathTex("A", font_size=40).move_to(line_min[0].get_left())
+        label_B.shift(0.3*LEFT)
+        label_A = MathTex("B", font_size=40).move_to(line_max[0].get_left())
+        label_A.align_to(label_B,LEFT)
 
         t = ValueTracker(x_max)
         initial_point = [axes.coords_to_point(t.get_value(), func(t.get_value()))]
@@ -35,18 +41,33 @@ class IntermediateValueTheorem(Scene):
         ).set_z_index(line_max.get_z_index())
         label_c =  MathTex("c", font_size=40)
         label_C = MathTex("C", font_size=40)
-        label_c.add_updater(lambda item: item.move_to(vertical_line.get_bottom()).shift(0.3*DOWN))
-        label_C.add_updater(lambda item: item.move_to(horizontal_line.get_left()).shift(0.3*LEFT))        
+        label_c.add_updater(
+            lambda item: item.move_to(vertical_line.get_bottom()).shift(0.3*DOWN)
+        )
+        label_C.add_updater(
+            lambda item: item.move_to(horizontal_line.get_left()).shift(0.3*LEFT)
+        )        
         
-        self.play(DrawBorderThenFill(axes), run_time =0.5, rate_func=rate_functions.ease_in_out_quad)
+        self.play(
+            DrawBorderThenFill(axes), 
+            run_time =0.5, rate_func=rate_functions.ease_in_out_quad
+        )
         self.play(Create(graph), run_time=0.754)
         self.play(FadeIn(label_a, label_b), run_time=0.75)
         self.play(Create(line_min[1]), Create(line_max[1]), run_time=0.7)
         self.play(FadeIn(dot_min, dot_max), run_time=0.5)
-        self.play(Create(line_min[0].rotate(PI)), Create(line_max[0].rotate(PI)), run_time =0.5)
+        self.play(
+            Create(line_min[0].rotate(PI)), 
+            Create(line_max[0].rotate(PI)), 
+            run_time =0.5
+        )
         self.play(FadeIn(label_A, label_B), run_time=0.75)
         self.play(Create(vertical_line), Create(horizontal_line), run_time=0.5)
-        self.play(FadeIn(dot, label_c, label_C), FadeOut(label_a, label_b, label_A, label_B), run_time=0.75)
+        self.play(
+            FadeIn(dot, label_c, label_C), 
+            FadeOut(label_a, label_b, label_A, label_B), 
+            run_time=0.75
+        )
         self.play(t.animate.set_value(x_min), run_time=1.2)
         self.play(t.animate.set_value(0.5*(x_min+x_max)), run_time=1)
         self.play(FadeIn(label_a, label_b, label_A, label_B), run_time=0.5)
