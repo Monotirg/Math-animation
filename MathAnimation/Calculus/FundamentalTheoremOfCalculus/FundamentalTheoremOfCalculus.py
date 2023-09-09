3e685eaafa3cf1ed444b67ca334312183514af49
Artem <144112818+Monotirg@users.noreply.github.com>
2023-09-09 23:02:20 +0300

    Update FundamentalTheoremOfCalculus.py

--- MathAnimation/Calculus/FundamentalTheoremOfCalculus/FundamentalTheoremOfCalculus.py
+++ MathAnimation/Calculus/FundamentalTheoremOfCalculus/FundamentalTheoremOfCalculus.py
@@ -19,16 +19,36 @@ class FundamentalTheoremOfCalculus(Scene):
             ]
         )
 
-        self.play(DrawBorderThenFill(axes), run_time=0.5, rate_func=rate_functions.ease_in_out_quad)
+        self.play(
+            DrawBorderThenFill(axes), 
+            run_time=0.5, rate_func=rate_functions.ease_in_out_quad
+        )
         self.play(Create(graph), run_time = 0.75)
-        self.play(DrawBorderThenFill(rect[0],rate_func=smooth), lag_ratio=0.5, run_time=0.6)
+        self.play(
+            DrawBorderThenFill(rect[0],rate_func=smooth), 
+            lag_ratio=0.5, run_time=0.6)
         self.wait(1)
-        self.play(ReplacementTransform(rect[0],rect[1], rate_func=rate_functions.ease_in_out_quart, lag_ratio=0.5), run_time=0.6)
+        self.play(
+            ReplacementTransform(
+                rect[0], rect[1], 
+                rate_func=rate_functions.ease_in_out_quart, 
+                lag_ratio=0.5
+            ), 
+            run_time=0.6
+        )
         self.wait(1)
         for i in range(1,len(rect)-1):
-            self.play(ReplacementTransform(rect[i],rect[i+1], rate_func=rate_functions.ease_in_out_quart, lag_ratio=0.5), run_time=0.6)
+            self.play(
+                ReplacementTransform(
+                    rect[i], rect[i+1], 
+                    rate_func=rate_functions.ease_in_out_quart, 
+                    lag_ratio=0.5
+                ), 
+                run_time=0.6
+            )
             
-        r = axes.get_area(graph, x_range=[1, 8], color="#005EFF", opacity=0.9, stroke_width=0).set_z_index(1)
+        r = axes.get_area(graph, x_range=[1, 8], color="#005EFF", opacity=0.9, stroke_width=0)
+        r.set_z_index(1)
         self.add(r)
         self.play(FadeOut(rect[-1]), lag_ratio=0.5)
         self.wait(2)
